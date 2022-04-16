a08db80c304c69be29f7e806e2b78e25aeb164e4
Masood <masoodraza75r@gmail.com>
2022-04-16 23:09:56 +0530

    Rehan Commmitted

--- Rehan.cpp
+++ Rehan.cpp
@@ -1,22 +1,3 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 2e5+5;
-vector<int> seive(){
-    vector<int> primes(N,0);
-
-    for(int i=2;i*i<N;++i){
-        for(int j=i*i;j<N;j+=i){
-            primes[j]=1;
-        }
-    }
-    vector<int> ans;
-    for(int i=0;i<N;++i){
-        if(primes[i]==0){
-            ans.push_back(i);
-        }
-    }
-    return ans;
-}
-int main(){
-    vector<int> ans=seive();
-}
